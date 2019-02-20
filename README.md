b0a7d14ee1e341a337712fb4e0c7ad64900a7195
Gary <13177748+oakgary@users.noreply.github.com>
2019-02-20 21:36:59 +0100

    refactoring

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ RedScrap gathers image and video urls from subreddits and posts them to a webhoo
 The urls that are to be gathered are configurable in reddits typical schema (e.g. top of the day).
 
 ## Setup
-RedScrap can be set up in many different ways, here is an example for the use with AWS Lambda.
 
 ### Requirements
 * AWS-Account ([Website](https://portal.aws.amazon.com/gp/aws/developer/registration/index.html))
@@ -15,7 +14,6 @@ RedScrap can be set up in many different ways, here is an example for the use wi
 
 1. **[Download](https://github.com/oakgary/RedScrap/archive/master.zip) and extract RedScrap**
 2. **`npm install`**
-3. **Replace scraper.js with [handler.js](https://gist.github.com/oakgary/47ed4e7635afcc27252cf350859ec3aa#file-handler-js) and create [serverless.yml](https://gist.github.com/oakgary/47ed4e7635afcc27252cf350859ec3aa#file-serverless-yml)**
 
 ### Configuration
 * config.json (sort and time are the configurations for the reddit posts)
@@ -29,22 +27,10 @@ RedScrap can be set up in many different ways, here is an example for the use wi
         "subreddits": "array of subreddits (e.g. ["funny","videos","gifs"]"
 }
 ```
-* serverless.yml
-
-```
-schedule indicates the time (in utc) of execution in cron format; current setup is daily at 6pm utc
-raise the timeout if you plan to post more than 100 urls with each execution
-```
-
-### Deployment
-
-1. **`serverless login`**
-2. **`serverless deploy`**
 
 ## Limits
 * Discord-Webhook: If you plan to post more than 30 urls per execution make sure to raise the sleep
- duration  
-(2s should be fine) in the scraper.js/handler.js's postUrl function in order not to trigger [Discords Rate-Limits](https://discordapp.com/developers/docs/topics/rate-limits).
+ duration (2s should be fine) in the scraper.js postUrl function in order not to trigger [Discords Rate-Limits](https://discordapp.com/developers/docs/topics/rate-limits).
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- scraper.js
+++ scraper.js
@@ -1,90 +1,67 @@
-const request = require('request');
 const fetch = require('node-fetch');
-const config = require("./config.json");
+const config = require('./config.json');
+
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const redditUrl = 'https://www.reddit.com/r/';
-const url_storage = new Map();
-const after_storage = new Map();
-
-function postWebhook(jsonBody) {
-    request({
-        url: config.webhook,
-        method: "POST",
-        headers: {
-            "content-type": "multipart/form-data",
-        },
-        json: true,
-        body: jsonBody
-    }, function (error, response, body) {
-        if (error) {
-            return console.error('Post on webhook failed ', error);
-        }
+const postToWebhook = async (body) => {
+  try {
+    const response = await fetch(config.webhook, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body,
     });
-}
+    if (!response.ok) throw new Error(response);
+  } catch (err) {
+    console.log('err: ', err);
+  }
+};
 
-//time = hour, day, week, month, year or all
-//sort = hot, new, rising, controversial or top 
-//items_per_page between 25 and 100 (included)
-async function loadSubreddits(subreddits) {
-    const arrOfarrOfUrls = await Promise.all(subreddits.map(async function (subreddit) {
-        const tmp = await getJson(subreddit, config.sort, config.time, config.items_per_page, config.amount_of_pages);
-        return tmp
-    }))
-    const urls = [...new Set([].concat(...arrOfarrOfUrls))];
-    postUrls(urls);
-}
-
-async function getJson(subreddit, sort, time, items_per_page, amount_of_pages) {
-    let urls = url_storage.get(subreddit);
-    for (let i = 0; i < amount_of_pages; i++) {
-        //Example URL: 'https://www.reddit.com/r/funny/top/.json?t=all&limit=100'
-        const url = redditUrl + subreddit + '/' + sort + '/.json?t=' + time + '&limit=' + items_per_page + '&after=' + after_storage.get(subreddit);
-        const response = await fetch(url);
-        const json = await response.json();
-        after_storage.set(subreddit, json.data.after);
-        urls = urls.concat(getUrlsFromJson(json));
-        url_storage.set(subreddit, urls);
-    }
-    return urls
-}
-
-function postUrls(urls) {
-    for (let i = 0; i < urls.length; i++) {
-        postUrl(urls[i], i);
-    }
-}
-
-async function postUrl(url, i) {
-    const jsonBody = { content: url };
+const postUrls = async (urls) => {
+  await Promise.all(urls.map(async (url, i) => {
+    const jsonBody = `{"content":"${url}"}`;
     await sleep(500 * i);
-    postWebhook(jsonBody);
-}
+    await postToWebhook(jsonBody);
+  }));
+};
 
-function getUrlsFromJson(jsonString) {
-    const urls = [];
-    jsonString.data.children.forEach(function (child) {
-        urls.push(child.data.url);
-    });
+const getUrls = async (subreddit, sort, time, itemsPerPage, amountOfPages, after, urls) => {
+  if (amountOfPages === 0) return urls;
+
+  const url = `https://www.reddit.com/r/${subreddit}/${sort}/.json?t=${time}&limit=${itemsPerPage}&after=${after || ''}`;
+  const response = await fetch(url);
+  const json = await response.json();
+  const { data: { children, after: newAfter } } = json;
+
+  const urlsFromJson = children.map(child => child.data.url);
+  const newUrls = urls.concat(urlsFromJson);
+
+  return getUrls(subreddit, sort, time, itemsPerPage, amountOfPages - 1, newAfter, newUrls);
+};
+
+const loadSubreddits = async (subreddits) => {
+  const arrOfarrOfUrls = await Promise.all(subreddits.map(async (subreddit) => {
+    const {
+      sort, time, itemsPerPage, amountOfPages,
+    } = config;
+    const urls = await getUrls(subreddit, sort, time, itemsPerPage, amountOfPages, null, []);
     return urls;
-}
+  }));
+  const urls = [...new Set([].concat(...arrOfarrOfUrls))];
+  postUrls(urls);
+};
 
-function init() {
-    if (JSON.stringify(config).includes(null)) {
-        console.error('config.json contains null values');
-        process.exitCode = 1;
-    } else {
-        if (config.subreddits.constructor === Array) {
-            config.subreddits.forEach(function (subreddit) {
-                url_storage.set(subreddit, []);
-                after_storage.set(subreddit, "");
-            });
-            loadSubreddits(config.subreddits);
-        } else {
-            console.error('config.subreddits has to be an array');
-            process.exitCode = 1;
-        }
-    }
-}
+const init = () => {
+  if (JSON.stringify(config).includes(null)) {
+    console.error('config.json contains null values');
+    process.exitCode = 1;
+  } else if (config.subreddits.constructor !== Array) {
+    console.error('config.subreddits has to be an array');
+    process.exitCode = 1;
+  } else {
+    loadSubreddits(config.subreddits);
+  }
+};
 
 init();
