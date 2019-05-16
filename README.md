47dd38e2e19316d46c4c7fa5814d131b906b11f8
Gary <13177748+oakgary@users.noreply.github.com>
2019-05-17 00:17:54 +0200

    refactorin

--- README.md
+++ README.md
@@ -14,18 +14,19 @@ The urls that are to be gathered are configurable in reddits typical schema (e.g
 * config.json (sort and time are the configurations for the reddit posts)
 ```
 {
-	"webhook": "your webhook url (e.g. for Discord: "https://discordapp.com/api/webhooks/{webhook.id}/{webhook.token})"",
-	"sort": "hot, new, rising, controversial or top",
-	"time": "hour, day, week, month, year or all",
-	"itemsPerPage": "between 1 and 100",
-	"amountOfPages": "basically a multiplicator for itemsPerPage",
-        "subreddits": "array of subreddits (e.g. ["funny","videos","gifs"]"
+    "webhook": "your webhook url (e.g. for Discord: "https://discordapp.com/api/webhooks/{webhook.id}/{webhook.token})"",
+    "sort": "hot, new, rising, controversial or top",
+    "time": "hour, day, week, month, year or all",
+    "itemsPerPage": "between 1 and 100",
+    "amountOfPages": "basically a multiplicator for itemsPerPage",
+    "subreddits": "array of subreddits (e.g. ["funny","videos","gifs"]",
+    "interval": interval between posts in milliseconds
 }
 ```
 
 ## Limits
-* Discord-Webhook: If you plan to post more than 30 urls per execution make sure to raise the sleep
- duration (2s should be fine) in the scraper.js postUrls function in order not to trigger [Discords Rate-Limits](https://discordapp.com/developers/docs/topics/rate-limits).
+* Discord-Webhook: If you plan to post more than 30 urls per execution make sure to raise the interval
+ duration (2s should be fine) in order not to trigger [Discords Rate-Limits](https://discordapp.com/developers/docs/topics/rate-limits).
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- scraper.js
+++ scraper.js
@@ -3,30 +3,25 @@ const config = require('./config.json');
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const postToWebhook = async (body) => {
-  try {
-    const response = await fetch(config.webhook, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body,
-    });
-    if (!response.ok) throw new Error(response);
-  } catch (err) {
-    console.log('err: ', err);
-  }
-};
-
-const postUrls = async (urls) => {
+const postUrlsToWebhook = async (urls, webhook, interval) => {
   await Promise.all(urls.map(async (url, i) => {
-    const jsonBody = `{"content":"${url}"}`;
-    await sleep(500 * i);
-    await postToWebhook(jsonBody);
+    await sleep(interval * i);
+    try {
+      const response = await fetch(webhook, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({ content: url }),
+      });
+      if (!response.ok) throw new Error(response);
+    } catch (err) {
+      console.log({ err });
+    }
   }));
 };
 
-const getUrls = async (subreddit, sort, time, itemsPerPage, amountOfPages, after, urls) => {
+const getUrlsFromSubreddit = async (subreddit, sort, time, itemsPerPage, amountOfPages, after, urls) => {
   if (amountOfPages === 0) return urls;
 
   const url = `https://www.reddit.com/r/${subreddit}/${sort}/.json?t=${time}&limit=${itemsPerPage}&after=${after || ''}`;
@@ -37,31 +32,27 @@ const getUrls = async (subreddit, sort, time, itemsPerPage, amountOfPages, after
   const urlsFromJson = children.map(child => child.data.url);
   const newUrls = urls.concat(urlsFromJson);
 
-  return getUrls(subreddit, sort, time, itemsPerPage, amountOfPages - 1, newAfter, newUrls);
+  return getUrlsFromSubreddit(subreddit, sort, time, itemsPerPage, amountOfPages - 1, newAfter, newUrls);
 };
 
-const loadSubreddits = async (subreddits) => {
+const getUrlsFromSubreddits = async (subreddits) => {
   const {
     sort, time, itemsPerPage, amountOfPages,
   } = config;
-  const arrOfarrOfUrls = await Promise.all(subreddits.map(async (subreddit) => {
-    const urls = await getUrls(subreddit, sort, time, itemsPerPage, amountOfPages, null, []);
-    return urls;
-  }));
-  const urls = [...new Set([].concat(...arrOfarrOfUrls))];
-  postUrls(urls);
+  const arrOfarrOfUrls = await Promise.all(subreddits.map(subreddit => getUrlsFromSubreddit(subreddit, sort, time, itemsPerPage, amountOfPages, null, [])));
+  return [...new Set([].concat(...arrOfarrOfUrls))];
 };
 
-const init = () => {
-  if (JSON.stringify(config).includes(null)) {
-    console.error('config.json contains null values');
-    process.exitCode = 1;
-  } else if (config.subreddits.constructor !== Array) {
-    console.error('config.subreddits has to be an array');
-    process.exitCode = 1;
-  } else {
-    loadSubreddits(config.subreddits);
-  }
-};
-
-init();
+if (JSON.stringify(config).includes(null)) {
+  console.error('config.json contains null values');
+  process.exitCode = 1;
+} else if (config.subreddits.constructor !== Array) {
+  console.error('config.subreddits has to be an array');
+  process.exitCode = 1;
+} else {
+  (async () => {
+    const { webhook, interval, subreddits } = config;
+    const urls = await getUrlsFromSubreddits(subreddits);
+    await postUrlsToWebhook(urls, webhook, interval);
+  })();
+}
